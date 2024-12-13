def9c1ee30cc1af64418b61d637f8a1f0762dd12
chiquixs <nafisachiquita06@gmail.com>
2024-12-13 09:54:21 +0700

    ini

---
+++ studyCase 1/tryThis.java
@@ -0,0 +1,12 @@
+public class tryThis{
+    
+    static int calculate(int cal){
+       int a = cal -1;
+       return a;
+    }
+    
+    public static void main(String[] args) {
+       
+        System.out.println(calculate(10));
+    }
+}
