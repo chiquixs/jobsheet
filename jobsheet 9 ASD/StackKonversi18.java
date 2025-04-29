595c4d618703e8c15a98eea540d02e0bfe4033e2
chiquixs <nafisachiquita06@gmail.com>
2025-04-29 14:51:34 +0700

    praktikum 1

--- jobsheet 9 ASD/StackKonversi18.java
+++ jobsheet 9 ASD/StackKonversi18.java
@@ -16,4 +16,24 @@ public class StackKonversi18{
     public boolean isFull(){
         return top == size -1;
     }
+
+    public void push (int data){
+        if (isFull()) {
+            System.out.println("stack penuh");
+        } else {
+            top++;
+            tumpukanBiner[top] = data;
+        }
+    }
+
+    public int pop(){
+        if (isEmpty()) {
+            System.out.println("stack kosong");
+            return -1;
+        } else {
+            int data = tumpukanBiner[top];
+            top--;
+            return data;
+        }
+    }
 }
